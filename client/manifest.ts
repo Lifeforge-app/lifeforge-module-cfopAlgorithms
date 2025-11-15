e9730f810c1327c26d5f755c4ef65a5c6e4c10fa
melvinchia3636 <melvinchia623600@gmail.com>
2025-11-15 21:08:10 +0800

    fix: fix routes config to match new API

--- client/manifest.ts
+++ client/manifest.ts
@@ -5,10 +5,10 @@ export default {
   name: 'CFOP Algorithms',
   icon: 'tabler:cube',
   routes: {
-    'cfop-algorithms': lazy(() => import('@')),
-    'cfop-algorithms/f2l': lazy(() => import('@/pages/F2L')),
-    'cfop-algorithms/oll': lazy(() => import('@/pages/OLL')),
-    'cfop-algorithms/pll': lazy(() => import('@/pages/PLL'))
+    '/': lazy(() => import('@')),
+    '/f2l': lazy(() => import('@/pages/F2L')),
+    '/oll': lazy(() => import('@/pages/OLL')),
+    '/pll': lazy(() => import('@/pages/PLL'))
   },
   category: 'Information'
 } satisfies ModuleConfig
